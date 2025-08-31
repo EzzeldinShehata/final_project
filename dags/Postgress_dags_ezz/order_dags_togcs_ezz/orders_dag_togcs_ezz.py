c7c5f6cfb2117b36139a39364b4672c2266e7a95
ezz <ezzeldin.shehata.fahmy@gmail.com>
2025-08-31 14:51:04 +0300

    updated connection

--- dags/Postgress_dags_ezz/order_dags_togcs_ezz/orders_dag_togcs_ezz.py
+++ dags/Postgress_dags_ezz/order_dags_togcs_ezz/orders_dag_togcs_ezz.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 # ---------------- Default Args ---------------- #
 default_args = {
@@ -16,9 +17,9 @@ default_args = {
 with DAG(
     dag_id="postgres_to_gcs_orders_products_ezz",
     default_args=default_args,
-    description="Export Postgres tables into GCS as CSV",
-    schedule_interval="30 12 * * *",  # runs daily at 3:30 PM Egypt (12:30 UTC)
-    start_date=datetime(2025, 8, 30),
+    description="Incremental export of Postgres tables into GCS",
+    schedule_interval="30 12 * * *",  # daily at 3:30 PM Egypt (12:30 UTC)
+    start_date=datetime(2025, 8, 22),
     catchup=False,
     tags=["postgres", "gcs", "etl"],
 ) as dag:
@@ -33,13 +34,22 @@ with DAG(
 
     BUCKET_NAME = "orders_products_ezzeldein"
 
+    # Build connection dynamically
+    POSTGRES_CONN_URI = "postgresql://postgres:Ready-de26@34.173.180.170:5432/postgres"
+
     for tbl in TABLES:
         PostgresToGCSOperator(
             task_id=f"postgres_to_gcs_{tbl}",
-            postgres_conn_id="DB1",  # Your Airflow Postgres connection
-            sql=f"SELECT * FROM public.{tbl};",
+            postgres_conn_id=None,  # we override with hook below
+            sql=f"""
+                SELECT * 
+                FROM public.{tbl}
+                WHERE updated_at_timestamp >= '{{{{ ds }}}}'
+            """,
             bucket=BUCKET_NAME,
-            filename=f"{tbl}-{{{{ ds_nodash }}}}.csv",  # Auto: table-YYYYMMDD.csv
+            filename=f"{tbl}/dt={{{{ ds_nodash }}}}/{tbl}-{{{{ ds_nodash }}}}.csv",
             export_format="csv",
             gzip=False,
+            # Here we inject the connection URI dynamically
+            postgres_hook=PostgresHook(postgres_conn_id=None, uri=POSTGRES_CONN_URI),
         )
